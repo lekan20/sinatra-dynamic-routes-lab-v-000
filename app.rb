d29395250942cf80b5c817a319c2ba43786ace29
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-03-19 23:45:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -21,9 +21,9 @@ class App < Sinatra::Base
     binding.pry
     if params[:operation] == "add"
       "#{params[:number1].to_i + params[:number2].to_i}"
-    elsif params[:operatoin] == "subtract"
+    elsif params[:operation] == "subtract"
       "#{params[:number2].to_i - params[:number1].to_i}"
-    elsif params[:operatoin] == "multiply"
+    elsif params[:operation] == "multiply"
       "#{params[:number1].to_i * params[:number2].to_i}"
     elsif params[:operation] == "divide"
       "#{params[:number1].to_i / params[:number2].to_i}"
