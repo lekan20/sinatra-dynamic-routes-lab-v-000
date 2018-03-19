19eae0d429e90cfb5d3decb3bda021e7ffaa526b
Lekan Nicholson <lekan.nicholson@gmail.com>
2018-03-19 23:39:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,7 +18,11 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    binding.pry
+    if params[:operation] == "add"
+      "#{params[:number1].to_i} + #{params[:number2].to_i}"
+    elsif params[:operation] == "divide"
+      "#{params[:number1].to_i}/#{params[:number2].to_i}"
+    end
   end
 
 end
